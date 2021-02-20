2897689de9f488177bbac6fd34b373f02f40a57c
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-02-20 19:44:53 -0300

    feat: isolating Input and Button Component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-import { FiLogIn, FiKey } from 'react-icons/fi';
+import { FiLogIn, FiKey, FiMail, FiLock } from 'react-icons/fi';
 
-import { Container, Content, BackgroundImg } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import Logo from '../../assets/logo.svg';
+import { Container, Content, BackgroundImg } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -14,10 +17,20 @@ const SignIn: React.FC = () => {
         <form>
           <h1>Faça seu logon</h1>
 
-          <input type="text" placeholder="seu e-mail" />
-          <input type="password" placeholder="sua senha" />
+          <Input
+            name="email"
+            icon={FiMail}
+            type="text"
+            placeholder="seu e-mail"
+          />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="sua senha"
+          />
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
 
           <a href="/forgot">
             <FiKey />
