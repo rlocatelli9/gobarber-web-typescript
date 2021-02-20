f44aba82c1bf917224769e5961d3699cd2e31953
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-02-20 19:59:48 -0300

    feat: SignUp Page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FiLogIn, FiKey, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -8,15 +8,17 @@ import Button from '../../components/Button';
 import Logo from '../../assets/logo.svg';
 import { Container, Content, BackgroundImg } from './styles';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
   return (
     <Container>
+      <BackgroundImg />
       <Content>
         <img src={Logo} alt="GoBarber" />
 
         <form>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu cadastro</h1>
 
+          <Input name="name" icon={FiUser} type="text" placeholder="seu nome" />
           <Input
             name="email"
             icon={FiMail}
@@ -30,22 +32,15 @@ const SignIn: React.FC = () => {
             placeholder="sua senha"
           />
 
-          <Button type="submit">Entrar</Button>
-
-          <a href="/forgot">
-            <FiKey />
-            Esqueci minha senha
-          </a>
+          <Button type="submit">Cadastrar</Button>
         </form>
         <a href="/register">
-          <FiLogIn size={20} />
-          Criar conta
+          <FiArrowLeft size={20} />
+          Voltar para logon
         </a>
       </Content>
-
-      <BackgroundImg />
     </Container>
   );
 };
 
-export default SignIn;
+export default SignUp;
