a45b552d6506b6fbd9b7fb155170a8cc08a59b19
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-02-21 11:10:13 -0300

    feat: create hook useAuth (custom hook)

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import * as Yup from 'yup';
 
 import { FormHandles } from '@unform/core';
@@ -9,7 +9,7 @@ import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 import Logo from '../../assets/logo.svg';
 import { Container, Content, BackgroundImg } from './styles';
@@ -22,7 +22,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useContext(AuthContext);
+  const { user, signIn } = useAuth();
 
   console.log(user);
 
