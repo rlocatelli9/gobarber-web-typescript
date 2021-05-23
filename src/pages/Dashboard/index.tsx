98837aad0cccb2e49777df4d2127d6aac72ae1fb
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-23 16:41:54 -0300

    feat: criando Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { FiPower } from 'react-icons/fi';
+import {
+  Container,
+  CustomHeader,
+  CustomHeaderContent,
+  CustomProfile,
+} from './styles';
+
+import logo from '../../assets/logo.svg';
+import { useAuth } from '../../context/AuthContext';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <CustomHeader>
+        <CustomHeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <CustomProfile>
+            <img src={user.avatarUrl} alt={user.name} />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </CustomProfile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </CustomHeaderContent>
+      </CustomHeader>
+    </Container>
+  );
 };
 
 export default Dashboard;
